790061491a06194268a9a679ed2199332c16b3b0
JoshDiaz <josethp1@hotmail.com>
2026-07-28 21:14:20 -0500

    Variables

---
+++ 01-Variables/Variables/Variables/Program.cs
@@ -0,0 +1,31 @@
+﻿//comentario de una linea
+
+/*comentario de varias lineas
+ * listas para validar el codigo
+ * como sabemos
+ */
+
+
+string nombre = "jose";     //explícitas
+var Url = "www.google.com"; //implícitas 
+
+
+//Variables
+int miNumeroEntero = 100;
+int otroNumeroEntero = -500;
+float miFloatNumero = 62.36f;
+double miDoubleNumero = 96.25;
+decimal miDecimal = 487.567m;
+byte miNumeroPequeno = 130; //porque ejemplo dias de la semana 7
+bool StatusProducto = true;
+string miCadena = "Esta es mi cadena de texto";
+char miCaracter = 'A';
+const int miConstanteNumerica = 500;
+
+//como evitar las variables nulls
+string? numeroNull = null;
+int? booleanoNull = null;
+
+
+
+Console.WriteLine("Hello, World!");
